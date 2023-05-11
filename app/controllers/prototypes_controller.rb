e0b9b5ac31312a863f4e9bcbd5c3f9828c2343db
rinoa349 <chan2s@icloud.com>
2023-05-11 19:06:56 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,13 +1,14 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 def index
   @prototypes = Prototype.includes(:user)
 end
 
 def show
-  @prototype = Prototype.find(params[:id])
   @comment = Comment.new
-  @comments = @prototype.comments.includes(:user)
+  @comments = @prototype.comments
 end
 
 def new
@@ -24,22 +25,23 @@ def create
 end
 
 def edit
-  @prototype = Prototype.find(params[:id])
 end
 
 def update
-  @prototype = Prototype.find(params[:id])
   if @prototype.update(prototype_params)
-    redirect_to prototype_path
+    redirect_to prototype_path(@prototype)
   else
     render :edit
   end
 end
 
 def destroy
-  prototype = Prototype.find(params[:id])
-  prototype.destroy
-  redirect_to root_path
+  if @prototype.destroy
+    redirect_to root_path
+  else
+    redirect_to root_path
+  end
+
 end
 
 private
@@ -47,9 +49,11 @@ def prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
 end
 
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
-  end
+def set_prototype
+  @prototype = Prototype.find(params[:id])
+end
+
+def contributor_confirmation
+  redirect_to root_path unless current_user == @prototype.user
 end
 end
